4fe7c44a164d91e573c56570d98fdad380dd5db4
rpog <Rafal.Pogorzelski@simcorp.com>
2024-10-07 22:08:17 +0200

    Revert "Added third message for console output"
    This reverts commit 3c0c96fda5b082eb9755accbb767d98ffcc05ff8.

--- MyConsoleApplication/MyConsoleApplication/Program.cs
+++ MyConsoleApplication/MyConsoleApplication/Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Learning git");
-Console.WriteLine("Feature branch 1 - development 2");
