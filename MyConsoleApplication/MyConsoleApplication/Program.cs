b4010b0ca3a5dc8854d78826bd36e427c4a4be2f
rpog <Rafal.Pogorzelski@simcorp.com>
2024-10-07 22:05:07 +0200

    Added third message for console output

--- MyConsoleApplication/MyConsoleApplication/Program.cs
+++ MyConsoleApplication/MyConsoleApplication/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Learning git");
+Console.WriteLine("Feature branch 1 - development 1");
